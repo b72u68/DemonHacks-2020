e4abf19b97165245d676b4e879822349f28c7f24
b72u68 <mdinh@hawk.iit.edu>
2020-10-25 05:53:58 -0500

    I'm going to sleep

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Homepage from "./components/Homepage/Homepage.js";
-/*
- *import DisplayExercises from "./components/DisplayExercises/DisplayExercises";
- */
+import ExerciseTutorial from "./components/ExerciseTutorial/ExerciseTutorial";
 
 export default class App extends Component {
   render() {
@@ -11,6 +9,7 @@ export default class App extends Component {
       <main>
         <Switch>
           <Route path="/" component={Homepage} exact />
+          <Route path="/tutorial" component={ExerciseTutorial} />
         </Switch>
       </main>
     );
